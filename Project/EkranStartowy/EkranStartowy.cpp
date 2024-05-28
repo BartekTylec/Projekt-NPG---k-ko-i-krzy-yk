5a94498b89732447b4a431021c989bf9484de90e
kubaowow <163141177+kubaowow@users.noreply.github.com>
2024-05-28 12:51:35 +0200

    Add files via upload

--- Project/EkranStartowy/EkranStartowy.cpp
+++ Project/EkranStartowy/EkranStartowy.cpp
@@ -3,8 +3,8 @@
 #include <vcl.h>
 #pragma hdrstop
 
-#include "Unit1.h"
-#include "Unit2.h"
+#include "EkranStartowy.h"
+#include "Menu.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -18,7 +18,7 @@ __fastcall TStart::TStart(TComponent* Owner)
 void __fastcall TStart::Button1Click(TObject *Sender)
 {
 			  Start->Hide();
-			  Tylec->ShowModal();
+			  Tylec->Show();
 }
 //---------------------------------------------------------------------------
 void __fastcall TStart::Button2Click(TObject *Sender)
@@ -30,7 +30,7 @@ void __fastcall TStart::Button2Click(TObject *Sender)
 
 void __fastcall TStart::StartCreate(TObject *Sender)
 {
-                    Image1->Picture->LoadFromFile("C:/Users/iwotr/Desktop/NPG/Graphics/XiO.bmp");
+                    Image1->Picture->LoadFromFile("C:/Users/dzien/Documents/builder/graphics/XiO.bmp");
 }
 //---------------------------------------------------------------------------
 
