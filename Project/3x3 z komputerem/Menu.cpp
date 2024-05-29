405f4068bd35eb1cc97a89edcbb3815a832484d5
BartekTylec <165039475+BartekTylec@users.noreply.github.com>
2024-05-29 14:47:50 +0200

    Add files via upload
    Ostetfczna werrsja

---
+++ Project/3x3 z komputerem/Menu.cpp
@@ -0,0 +1,113 @@
+
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Menu.h"
+#include "ThrByThr.h"
+#include "FoByFo.h"
+#include "FivByFive.h"
+ #include "ThrByThrCompX.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TTylec *Tylec;
+int x = 0;
+std::string* w;
+//---------------------------------------------------------------------------
+__fastcall TTylec::TTylec(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TTylec::Button2Click(TObject *Sender)
+{
+		   x += 2;
+		   Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button1Click(TObject *Sender)
+{
+
+		   x += 1;
+           Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
+
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button5Click(TObject *Sender)
+{
+
+		   x += 20;
+		   Button5->Enabled = False;
+		   Button4->Enabled = False;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button4Click(TObject *Sender)
+{
+
+		   x += 10;
+           Button5->Enabled = False;
+		   Button4->Enabled = False;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button3Click(TObject *Sender)
+{
+		   x +=3;
+		   Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
+}
+//---------------------------------------------------------------------------
+
+
+void __fastcall TTylec::Button6Click(TObject *Sender)
+{
+	if (x == 11) {
+		 Form2->ShowModal();
+		 Tylec->Close();
+	}
+	if (x == 12) {
+	   FourByFour->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 13) {
+		FiveByFive->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 21) {
+		Form3->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 22) {
+		 ShowMessage("Work in progress, opcja niedostêpna");
+	}
+	if (x == 23) {
+		ShowMessage("Work in progress, opcja niedostepna:(");
+	}
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button7Click(TObject *Sender)
+{
+	x = 0;
+	Button1->Enabled = True;
+	Button2->Enabled = True;
+	Button3->Enabled = True;
+	Button4->Enabled = True;
+	Button5->Enabled = True;
+
+
+}
+//---------------------------------------------------------------------------
+
---
+++ Project/3x3 z komputerem/Menu.dfm
@@ -0,0 +1,103 @@
+object Tylec: TTylec
+  Left = 0
+  Top = 0
+  Caption = 'Menu'
+  ClientHeight = 412
+  ClientWidth = 592
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Label1: TLabel
+    Left = 160
+    Top = 88
+    Width = 288
+    Height = 37
+    Caption = 'Wybierz rozmiar planszy'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -27
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Label2: TLabel
+    Left = 184
+    Top = 248
+    Width = 224
+    Height = 37
+    Caption = 'Wybiez tryb graczy'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -27
+    Font.Name = 'Segoe UI'
+    Font.Style = []
+    ParentFont = False
+  end
+  object Button2: TButton
+    Left = 256
+    Top = 152
+    Width = 97
+    Height = 25
+    Caption = '4x4'
+    TabOrder = 0
+    OnClick = Button2Click
+  end
+  object Button1: TButton
+    Left = 136
+    Top = 152
+    Width = 97
+    Height = 25
+    Caption = '3x3'
+    TabOrder = 1
+    OnClick = Button1Click
+  end
+  object Button3: TButton
+    Left = 376
+    Top = 152
+    Width = 97
+    Height = 25
+    Caption = '5x5'
+    TabOrder = 2
+    OnClick = Button3Click
+  end
+  object Button4: TButton
+    Left = 168
+    Top = 320
+    Width = 105
+    Height = 25
+    Caption = 'Dw'#243'ch graczy'
+    TabOrder = 3
+    OnClick = Button4Click
+  end
+  object Button5: TButton
+    Left = 327
+    Top = 320
+    Width = 105
+    Height = 25
+    Caption = 'Z automatem'
+    TabOrder = 4
+    OnClick = Button5Click
+  end
+  object Button6: TButton
+    Left = 216
+    Top = 368
+    Width = 169
+    Height = 25
+    Caption = 'Rozpocznij gr'#281
+    TabOrder = 5
+    OnClick = Button6Click
+  end
+  object Button7: TButton
+    Left = 464
+    Top = 24
+    Width = 107
+    Height = 25
+    Caption = 'Resetuj ustawienia'
+    TabOrder = 6
+    OnClick = Button7Click
+  end
+end
