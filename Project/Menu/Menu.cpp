5f52f091e9e9f2e0fc29d302f15a8ec4bc39e2ff
kubaowow <163141177+kubaowow@users.noreply.github.com>
2024-05-29 20:48:15 +0200

    ostateczna

--- Project/Menu/Menu.cpp
+++ Project/Menu/Menu.cpp
@@ -1,3 +1,4 @@
+﻿
 //---------------------------------------------------------------------------
 
 #include <vcl.h>
@@ -7,10 +8,13 @@
 #include "ThrByThr.h"
 #include "FoByFo.h"
 #include "FivByFive.h"
+ #include "ThrByThrCompX.h"
+ #include "EkranStartowy.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TTylec *Tylec;
+int x = 0;
 //---------------------------------------------------------------------------
 __fastcall TTylec::TTylec(TComponent* Owner)
 	: TForm(Owner)
@@ -19,37 +23,95 @@ __fastcall TTylec::TTylec(TComponent* Owner)
 //---------------------------------------------------------------------------
 void __fastcall TTylec::Button2Click(TObject *Sender)
 {
-		   Tylec->Close();
-           FourByFour->ShowModal();
+		   x += 2;
+		   Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TTylec::Button1Click(TObject *Sender)
 {
 
-		   Tylec->Close();
-		   Form2->ShowModal();
+		   x += 1;
+           Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
+
+
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TTylec::Button5Click(TObject *Sender)
 {
 
-		   Tylec->Close();
+		   x += 20;
+		   Button5->Enabled = False;
+		   Button4->Enabled = False;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TTylec::Button4Click(TObject *Sender)
 {
 
-		   Tylec->Close();
+		   x += 10;
+           Button5->Enabled = False;
+		   Button4->Enabled = False;
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TTylec::Button3Click(TObject *Sender)
 {
-		   Tylec->Close();
-		   FiveByFive->ShowModal();
+		   x +=3;
+		   Button1->Enabled = False;
+		   Button2->Enabled = False;
+		   Button3->Enabled = False;
 }
 //---------------------------------------------------------------------------
 
+
+void __fastcall TTylec::Button6Click(TObject *Sender)
+{
+	if (x == 11) {
+		 Form2->ShowModal();
+		 Tylec->Close();
+	}
+	if (x == 12) {
+	   FourByFour->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 13) {
+		FiveByFive->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 21) {
+		Form3->ShowModal();
+		Tylec->Close();
+	}
+	if (x == 22) {
+		 ShowMessage("Work in progress, opcja niedostêpna");
+	}
+	if (x == 23) {
+		ShowMessage("Work in progress, opcja niedostepna:(");
+	}
+}
+
+//---------------------------------------------------------------------------
+
+void __fastcall TTylec::Button7Click(TObject *Sender)
+{
+	x = 0;
+	Button1->Enabled = True;
+	Button2->Enabled = True;
+	Button3->Enabled = True;
+	Button4->Enabled = True;
+	Button5->Enabled = True;
+
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TTylec::OnClose(TObject *Sender)
+{
+		Tylec->Close();
+		Start->Close();
+}
--- Project/Menu/Menu.h
+++ Project/Menu/Menu.h
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------
 
-#ifndef Unit2H
-#define Unit2H
+#ifndef MenuH
+#define MenuH
 //---------------------------------------------------------------------------
 #include <System.Classes.hpp>
 #include <Vcl.Controls.hpp>
@@ -11,19 +11,25 @@
 class TTylec : public TForm
 {
 __published:	// IDE-managed Components
-	TButton *Button1;
 	TButton *Button2;
+	TButton *Button1;
 	TButton *Button3;
 	TLabel *Label1;
 	TLabel *Label2;
 	TButton *Button4;
 	TButton *Button5;
-	void __fastcall Button2Click(TObject *Sender);
+	TButton *Button6;
+	TButton *Button7;
 	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
 	void __fastcall Button3Click(TObject *Sender);
 	void __fastcall Button4Click(TObject *Sender);
 	void __fastcall Button5Click(TObject *Sender);
+	void __fastcall Button6Click(TObject *Sender);
+	void __fastcall Button7Click(TObject *Sender);
+    void __fastcall OnClose(TObject *Sender);
 private:	// User declarations
+void TTylec::reset();
 public:		// User declarations
 	__fastcall TTylec(TComponent* Owner);
 };
