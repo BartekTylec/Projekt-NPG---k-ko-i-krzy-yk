cd3587cfa7fdfde48475766388d9c7c4e6a7e7d8
kubaowow <163141177+kubaowow@users.noreply.github.com>
2024-05-29 20:54:48 +0200

    ostateczna

--- Project/Project1.cpp
+++ Project/Project1.cpp
@@ -4,10 +4,12 @@
 #pragma hdrstop
 #include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("ThrByThr.cpp", Form2);
-USEFORM("FoByFo.cpp", FourByFour);
 USEFORM("Menu.cpp", Tylec);
+USEFORM("ThrByThr.cpp", Form2);
 USEFORM("EkranStartowy.cpp", Start);
+USEFORM("FivByFive.cpp", FiveByFive);
+USEFORM("FoByFo.cpp", FourByFour);
+USEFORM("ThrByThrCompX.cpp", Form3);
 //---------------------------------------------------------------------------
 int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
@@ -19,6 +21,8 @@ int WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 		Application->CreateForm(__classid(TTylec), &Tylec);
 		Application->CreateForm(__classid(TForm2), &Form2);
 		Application->CreateForm(__classid(TFourByFour), &FourByFour);
+		Application->CreateForm(__classid(TFiveByFive), &FiveByFive);
+		Application->CreateForm(__classid(TForm3), &Form3);
 		Application->Run();
 	}
 	catch (Exception &exception)
